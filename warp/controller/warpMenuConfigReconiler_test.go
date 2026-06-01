812e74b4530dbd88502136d23486766c32682f19
Henry Bobka <henry.bobka@cloudogu.com>
2026-06-01 09:28:14 +0200

    #21: Remove obsolete tag-based dogu reconcile test

--- warp/controller/warpMenuConfigReconiler_test.go
+++ warp/controller/warpMenuConfigReconiler_test.go
@@ -6,9 +6,6 @@ import (
 	"os"
 	"testing"
 
-	"github.com/cloudogu/ces-commons-lib/dogu"
-	"github.com/cloudogu/cesapp-lib/core"
-	config2 "github.com/cloudogu/k8s-registry-lib/config"
 	warpmenu "github.com/cloudogu/k8s-warp-menu-entry-lib/api/v1"
 	"github.com/cloudogu/warp-assets/config"
 	"github.com/stretchr/testify/assert"
@@ -19,7 +16,6 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/yaml"
 )
@@ -95,55 +91,6 @@ func TestWarpMenuReconcile(t *testing.T) {
 		assert.ElementsMatch(t, adminExpectedWarpMenuEntries, adminWarpMenuCategory.Entries)
 
 	})
-
-	t.Run("should not create a menu entry for a dogu that doesn't have the right tag", func(t *testing.T) {
-		clientMock := newMockK8sClient(t)
-		globalConfigRepoMock := NewMockGlobalConfigRepository(t)
-		doguVersionRegistryMock := NewMockDoguVersionRegistry(t)
-		localDoguRepo := NewMockLocalDoguRepo(t)
-		warpMenuPath := t.TempDir()
-		eventRecorderMock := newMockEventRecorder(t)
-
-		eventRecorderMock.EXPECT().Event(mock.Anything, corev1.EventTypeNormal, warpMenuUpdateEventReason, "Warp menu updated.")
-
-		warpMenuConfig := config.Configuration{
-			Sources: []config.Source{
-				{
-					Path: "/dogu",
-					Type: "dogus",
-					Tag:  "show_in_warp_menu",
-				},
-			},
-		}
-		mockExpectGetWarpMenuConfig(t, clientMock, warpMenuConfig)
-
-		globalConfig := config2.CreateGlobalConfig(config2.Entries{})
-		globalConfigRepoMock.EXPECT().Get(mock.Anything).Return(globalConfig, nil)
-
-		dogus := []*core.Dogu{
-			{
-				Name:        "repo/dogu_1",
-				Version:     "1.0.0-1",
-				DisplayName: "Dogu 1",
-				Description: "Dogu 1 Description",
-				Category:    "DevApps",
-				Tags:        []string{"tag_does_not_match_tag_in_warp_menu_config"},
-			},
-		}
-
-		doguSimpleVersionNames, simpleVersionNameToDoguMap := newSimpleNameToDoguMap(t, dogus)
-		doguVersionRegistryMock.EXPECT().GetCurrentOfAll(mock.Anything).Return(doguSimpleVersionNames, nil)
-		localDoguRepo.EXPECT().GetAll(mock.Anything, doguSimpleVersionNames).Return(simpleVersionNameToDoguMap, nil)
-
-		reconciler := NewWarpMenuReconciler(clientMock, eventRecorderMock, warpMenuPath, testDeploymentName)
-
-		request := ctrl.Request{NamespacedName: types.NamespacedName{Namespace: "aNamespace", Name: "aConfigMap"}}
-		_, err := reconciler.Reconcile(context.Background(), request)
-		require.NoError(t, err)
-
-		warpMenuCategories := parseWarpMenuCategoriesFromJsonFile(t, warpMenuPath)
-		assert.Equal(t, 0, len(warpMenuCategories))
-	})
 }
 
 func newClientBuilder(t *testing.T) *fake.ClientBuilder {
@@ -182,22 +129,6 @@ func getConfigMap(t *testing.T, warpMenuConfig config.Configuration) *corev1.Con
 	return &configMap
 }
 
-func mockExpectGetWarpMenuConfig(t *testing.T, clientMock *mockK8sClient, warpMenuConfig config.Configuration) {
-	clientMock.EXPECT().
-		Get(mock.Anything, mock.Anything, mock.AnythingOfType("*v1.ConfigMap")).
-		Run(func(ctx context.Context, key types.NamespacedName, obj client.Object, opts ...client.GetOption) {
-			warpMenuConfigAsString, err := yaml.Marshal(warpMenuConfig)
-			require.NoError(t, err)
-
-			configMap := obj.(*corev1.ConfigMap)
-			data := map[string]string{
-				"warp": string(warpMenuConfigAsString),
-			}
-			configMap.Data = data
-		}).
-		Return(nil)
-}
-
 func findCategoryByTitle(warpMenuCategories []WarpMenuCategory, title string) (WarpMenuCategory, bool) {
 	for _, cat := range warpMenuCategories {
 		if cat.Title == title {
@@ -205,21 +136,4 @@ func findCategoryByTitle(warpMenuCategories []WarpMenuCategory, title string) (W
 		}
 	}
 	return WarpMenuCategory{}, false
-}
-
-func newSimpleNameToDoguMap(t *testing.T, dogus []*core.Dogu) ([]dogu.SimpleNameVersion, map[dogu.SimpleNameVersion]*core.Dogu) {
-	var nameVersions []dogu.SimpleNameVersion
-	doguMap := make(map[dogu.SimpleNameVersion]*core.Dogu)
-	for _, d := range dogus {
-		version, err := core.ParseVersion(d.Version)
-		require.NoError(t, err)
-		nameVersion := dogu.SimpleNameVersion{
-			Name:    dogu.SimpleName(d.Name),
-			Version: version,
-		}
-		nameVersions = append(nameVersions, nameVersion)
-		doguMap[nameVersion] = d
-	}
-
-	return nameVersions, doguMap
 }
